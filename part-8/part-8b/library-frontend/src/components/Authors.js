3ed2d31405380e013397ea08a07c55b204efe061
elsilv <elsi.silvennoinen@gmail.com>
2021-11-03 15:49:27 +0200

    Tehtävä 8.8

---
+++ part-8/part-8b/library-frontend/src/components/Authors.js
@@ -0,0 +1,44 @@
+import { useQuery } from '@apollo/client'
+import React from 'react'
+import { ALL_AUTHORS } from '../queries'
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+
+  if(result.loading) {
+    return (<div>loading...</div>)
+  }
+  
+  if (!props.show) {
+    return null
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {result.data.allAuthors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
