cbe3b9b30ec1b7c4141b6caee197ace98a1f86c1
elsilv <elsi.silvennoinen@gmail.com>
2021-11-22 18:37:30 +0200

    Tehtävä 8.23

--- part-8/part-8a/library-backend.js
+++ part-8/part-8a/library-backend.js
@@ -1,15 +1,21 @@
-const { ApolloServer, gql, UserInputError, AuthenticationError } = require('apollo-server')
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
+const { createServer } = require("http");
+const express = require("express");
+const { execute, subscribe } = require("graphql");
+const { ApolloServer, gql } = require("apollo-server-express");
+const { PubSub } = require("graphql-subscriptions");
+const { SubscriptionServer } = require("subscriptions-transport-ws");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 
-const JWT_SECRET = 'secret'
+const mongoose = require('mongoose');
 
-const Book = require('./models/book')
-const Author = require('./models/author')
-const User = require('./models/user')
+const Book = require('./models/book');
+const Author = require('./models/author');
+const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://kissa:7xA3py17FiIpHHUL@cluster0.gvas4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-console.log('connecting to', MONGODB_URI)
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'secret';
+const MONGODB_URI = 'mongodb+srv://kissa:7xA3py17FiIpHHUL@cluster0.gvas4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+console.log('connecting to', MONGODB_URI);
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
@@ -17,97 +23,23 @@ mongoose.connect(MONGODB_URI)
   })
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message)
-  })
+  });
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
+(async () => {
+  const PORT = 4000;
+  const pubsub = new PubSub();
+  const app = express();
+  const httpServer = createServer(app);
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
-
-const typeDefs = gql`
+  const typeDefs = gql`
   type User {
     username: String!
     favoriteGenre: String!
     id: ID!
   }
-
   type Token {
     value: String!
   }
-
   type Book {
       title: String!
       published: Int!
@@ -115,14 +47,12 @@ const typeDefs = gql`
       genres: [String!]!
       id: ID!
   }
-
   type Author {
       name: String!
       born: Int
       bookCount: Int
       id: ID!
   }
-
   type Mutation {
       addBook(
           title: String!
@@ -143,7 +73,6 @@ const typeDefs = gql`
         password: String!
       ): Token
   }
-
   type Query {
       bookCount: Int!
       authorCount: Int!
@@ -151,9 +80,13 @@ const typeDefs = gql`
       allAuthors: [Author!]!
       me: User
   }
-`
 
-const resolvers = {
+  type Subscription {
+    bookAdded: Book!
+  }
+  `;
+
+  const resolvers = {
     Query: {
       bookCount: () => Book.collection.countDocuments(),
       authorCount: () => Author.collection.countDocuments(),
@@ -196,6 +129,7 @@ const resolvers = {
       addBook: async (root, args, context) => {
         const author = await Author.findOne({ name: args.author })
         const currentUser = context.currentUser
+        const book = new Book({ ...args })
 
         if(!currentUser) {
           throw new AuthenticationError("not authenticated")
@@ -206,12 +140,7 @@ const resolvers = {
         }
   
         try {
-          return await new Book({
-            title: args.title,
-            published: args.published,
-            author,
-            genres: args.genres
-          }).save()
+           await book.save()
            await currentUser.save() 
         } catch(error) {
             throw new UserInputError(error.message, {
@@ -219,6 +148,8 @@ const resolvers = {
           })
         }
   
+          pubsub.publish('BOOK_ADDED', { bookAdded: book })
+          return book
         },
 
         editAuthor: async (root, args, context) => {
@@ -270,26 +201,31 @@ const resolvers = {
       }
 
       return { value: jwt.sign(userForToken, JWT_SECRET) }
-    }
-  
-  }
-}
+    },
+  },
+  Subscription: {
+    bookAdded: {
+      subscribe: () => pubsub.asyncIterator(["BOOK_ADDED"]),
+    },
+  },
+  };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null
-    if (auth && auth.toLowerCase().startsWith('bearer ')) {
-      const decodedToken = jwt.verify(
-        auth.substring(7), JWT_SECRET
-      )
-      const currentUser = await User.findById(decodedToken.id)
-      return { currentUser }
-    }
-  }
-})
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+  const server = new ApolloServer({
+    schema,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  SubscriptionServer.create(
+    { schema, execute, subscribe },
+    { server: httpServer, path: server.graphqlPath }
+  );
+
+  httpServer.listen(PORT, () => {
+    console.log(`Server ready at localhost:${PORT}${server.graphqlPath}`);
+    console.log(`Subscriptions ready at ws://localhost:${PORT}${server.graphqlPath}`);
+  });
+
+})();
